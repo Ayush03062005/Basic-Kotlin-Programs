4c5cc26184ff41d816d7e97adea5e6cd0b8a8a6e
Ayush Ku. Singh <73843758+Ayush03062005@users.noreply.github.com>
2026-05-28 15:24:17 +0530

    swapping w/o 3rd var

---
+++ q12b.kt
@@ -0,0 +1,11 @@
+//Q12.b
+fun main()
+{
+    var a=10
+    var b=20
+    println("Before Swapping, a= $a and b= $b")
+    a=a+b
+    b=a-b
+    a=a-b
+    println("After Swapping, a= $a and b= $b")
+}
