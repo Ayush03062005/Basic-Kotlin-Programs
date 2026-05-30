d6d7b5301ffb8880fe832b73b1fc776b7ffd0d19
Ayush Ku. Singh <73843758+Ayush03062005@users.noreply.github.com>
2026-05-30 16:00:09 +0530

    Add coroutine-based weather fetching example

---
+++ A2/q18.kt
@@ -0,0 +1,11 @@
+import kotlinx.coroutines.*
+
+suspend fun fetchWeather(): String {
+    delay(1000)
+    return "Sunny"
+}
+
+fun main() = runBlocking {
+    val weather = fetchWeather()
+    println(weather)
+}
